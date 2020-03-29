f1ef193ae6ae7a68d0e2dcc8a58d0d78f635a2e8
harrydry2 <harrydry1996@gmail.com>
2020-03-29 19:31:00 +0100

    s

--- src/modules/eoSubmit.js
+++ src/modules/eoSubmit.js
@@ -6,38 +6,46 @@ function validateEmail(email) {
   return re.test(email);
 }
 
-function submitMail(button, inputName, inputEmail, inputSchool, inputDesc, red) {
-  button.on('click', async (e) => {
+function submitMail(
+  button,
+  inputName,
+  inputEmail,
+  inputSchool,
+  inputDesc,
+  red
+) {
+  button.on("click", async e => {
     e.preventDefault();
-    red.style.display = 'block';
+    red.style.display = "block";
     if (validateEmail(inputEmail.value)) {
-      const { data } = await axios.post('/api/subscribe', {
+      const { data } = await axios.post("/api/subscribe", {
         name: inputName.value,
         email: inputEmail.value,
         school: inputSchool.value,
         desc: inputDesc.value
       });
-      if (data.email === 'true') {
-        red.style.color = '#00c26e';
+      if (data.email === "true") {
+        red.style.color = "#00c26e";
         red.innerText = `We'll be in touch within 24hrs`;
+        red.classList.add("gta__1");
       }
     } else {
-      red.style.color = '#D0021B';
-      red.innerText = 'Not a valid email. Try again.';
+      red.style.color = "#D0021B";
+      red.innerText = "Not a valid email. Try again.";
     }
-    inputName.value = '';
-    inputEmail.value = '';
-    inputSchool.value = '';
-    inputDesc.value = '';
+    inputName.value = "";
+    inputEmail.value = "";
+    inputSchool.value = "";
+    inputDesc.value = "";
   });
 }
 
 export function mailSubmitFromHome() {
-  const button = $('.eoSubmit');
-  const inputName = $('.inputName');
-  const inputEmail = $('.inputEmail');
-  const inputSchool = $('.inputSchool');
-  const inputDesc = $('.inputDesc');
-  const red = $('.git__submit-red');
+  const button = $(".eoSubmit");
+  const inputName = $(".inputName");
+  const inputEmail = $(".inputEmail");
+  const inputSchool = $(".inputSchool");
+  const inputDesc = $(".inputDesc");
+  const red = $(".git__submit-red");
   submitMail(button, inputName, inputEmail, inputSchool, inputDesc, red);
 }
